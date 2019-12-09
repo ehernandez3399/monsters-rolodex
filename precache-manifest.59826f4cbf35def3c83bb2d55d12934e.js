ccfb1e3d9aea2687cd4f67ecc69a072cca70b823
ehernandez3399 <32855281+ehernandez3399@users.noreply.github.com>
2019-12-08 20:46:31 -0600

    Updates

---
+++ precache-manifest.59826f4cbf35def3c83bb2d55d12934e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c81033032c9aa0185166cacb954330c",
+    "url": "/monsters-rolodex.git/index.html"
+  },
+  {
+    "revision": "102125f5f679b0fa2822",
+    "url": "/monsters-rolodex.git/static/css/main.50b037b0.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex.git/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex.git/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "102125f5f679b0fa2822",
+    "url": "/monsters-rolodex.git/static/js/main.111a2751.chunk.js"
+  },
+  {
+    "revision": "0639c29fd42b9b857e8b",
+    "url": "/monsters-rolodex.git/static/js/runtime-main.6a13e550.js"
+  }
+]);
